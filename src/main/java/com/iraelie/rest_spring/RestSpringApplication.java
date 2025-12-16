fa95e24b080b11ca3ebd4bdbdcb40a4fed39b63f
Elie <niyubwayoiraelie5777@gmail.com>
2025-12-16 21:06:53 +0200

    marked our entry point as REST API controller.

--- src/main/java/com/iraelie/rest_spring/RestSpringApplication.java
+++ src/main/java/com/iraelie/rest_spring/RestSpringApplication.java
@@ -2,12 +2,18 @@ package com.iraelie.rest_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestSpringApplication.class, args);
 	}
-
+	@GetMapping
+	public String helloWorld() {
+		return "Hello spring boot";
+	}
 }
