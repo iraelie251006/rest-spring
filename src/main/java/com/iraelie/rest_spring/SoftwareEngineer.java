abce61c07a4c6ee30bc987f10f226fe415010387
Elie <niyubwayoiraelie5777@gmail.com>
2025-12-18 18:27:55 +0200

    Made ID UUID auto-generated for unique primary key.

--- src/main/java/com/iraelie/rest_spring/SoftwareEngineer.java
+++ src/main/java/com/iraelie/rest_spring/SoftwareEngineer.java
@@ -1,6 +1,8 @@
 package com.iraelie.rest_spring;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.Objects;
 public class SoftwareEngineer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Integer id;
     private String name;
     private List<String> techStack;
