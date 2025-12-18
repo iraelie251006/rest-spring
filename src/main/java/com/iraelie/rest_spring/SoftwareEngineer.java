4f1d30391f690da01e8af8400f2c6b5646427936
Elie <niyubwayoiraelie5777@gmail.com>
2025-12-18 17:58:12 +0200

    Turned SoftwareEngineer class into table using Spring Data JPA

--- src/main/java/com/iraelie/rest_spring/SoftwareEngineer.java
+++ src/main/java/com/iraelie/rest_spring/SoftwareEngineer.java
@@ -1,9 +1,15 @@
 package com.iraelie.rest_spring;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.util.List;
 import java.util.Objects;
 
+@Entity
 public class SoftwareEngineer {
+
+    @Id
     private Integer id;
     private String name;
     private List<String> techStack;
