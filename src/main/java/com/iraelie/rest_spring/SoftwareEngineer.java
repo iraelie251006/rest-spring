1aeba44e481633c8aac89d73d4d2147e84d89873
Elie <niyubwayoiraelie5777@gmail.com>
2025-12-18 18:32:07 +0200

    updated constraints to fix UUID error.

--- src/main/java/com/iraelie/rest_spring/SoftwareEngineer.java
+++ src/main/java/com/iraelie/rest_spring/SoftwareEngineer.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class SoftwareEngineer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private List<String> techStack;
